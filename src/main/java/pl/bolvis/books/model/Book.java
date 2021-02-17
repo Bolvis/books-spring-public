486dd4c1513e32184cc1ad251f6fe1e84b384071
Dawid <dawid.stasiak17@gmail.com>
2021-02-17 17:19:28 +0100

    change data to getter and setter

--- src/main/java/pl/bolvis/books/model/Book.java
+++ src/main/java/pl/bolvis/books/model/Book.java
@@ -1,14 +1,15 @@
 package pl.bolvis.books.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-
+@Getter
+@Setter
 @Entity(name = "books")
-public @Data
-class Book {
+public class Book {
 
     @Id
     private String isbn;
